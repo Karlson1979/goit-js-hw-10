51ad64596edd76ebf6580cef0b0657c8da7faeaf
Tochka <tochka114@gmail.com>
2024-06-11 23:00:42 +0300

    the end

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,5 +1,41 @@
-
 import iziToast from "izitoast";
-
 import "izitoast/dist/css/iziToast.min.css";
 
+
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(formEl);
+  const delay = parseInt(formData.get('delay'), 10);
+  const state = formData.get('state');
+  const status = state === 'fulfilled';
+
+  createPromise(delay, status)
+    .then((delay) => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch((delay) => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+});
+
+function createPromise(delay, status) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (status) {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
